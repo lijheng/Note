c114eb8ca026d2146268367e659b9bf72adabe54
lijheng <lijheng@users.noreply.github.com>
2019-08-26 21:20:50 +0800

    新建项目

---
+++ src/main/java/com/summer/note/task/TaskActivity.java
@@ -0,0 +1,16 @@
+package com.summer.note.task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.summer.note.R;
+
+public class TaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_task);
+    }
+}
