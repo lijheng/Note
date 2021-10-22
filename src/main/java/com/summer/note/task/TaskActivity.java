841f12657c2f8c8e62d94b75facb8fe7fd68b65c
summer <1107285162@qq.com>
2021-10-22 21:45:20 +0800

    完善NoneCompressLayout及增加示例

--- src/main/java/com/summer/note/task/TaskActivity.java
+++ src/main/java/com/summer/note/task/TaskActivity.java
@@ -1,19 +1,28 @@
 package com.summer.note.task;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.summer.note.R;
 
 public class TaskActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_task);
-        RecyclerView recyclerView;
+        initRecyclerView();
     }
+
+    private void initRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.act_task_rv_data);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        recyclerView.setAdapter(new FlowerAdapter());
+//        recyclerView.set
+    }
+
 }
